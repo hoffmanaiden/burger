ba52a6351a32f54d14d856147971af998271fb59
hoffmanaiden <hoffman.aiden@gmail.com>
2021-05-31 13:25:03 -0400

    simple 3D burger

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import * as THREE from 'three'
 import './App.css';
+import React, { Suspense, useRef, useState } from 'react';
+import { Canvas, useLoader } from '@react-three/fiber'
+import { useGLTF, OrbitControls } from '@react-three/drei'
+
+function Burger() {
+  const { nodes, materials } = useGLTF("/burger.glb")
+  console.log(nodes)
+  return (
+    <group>
+      <group scale={[0.25, 0.25, 0.25]}>
+        <mesh name="topBun" geometry={nodes.top_bun.geometry} material={materials.Bun} position={[0,1,0]}/>
+        <mesh name="cheese" geometry={nodes.cheese.geometry} material={materials.cheese} position={[0,-0.25,0]}/>
+        <mesh name="meat" geometry={nodes.meat.geometry} material={materials.meat} position={[0,0,0]}/>
+        <mesh name="bottomBun" geometry={nodes.bottom_bun.geometry} material={materials.Bun} position={[0,-2,0]}/>
+      </group>
+    </group>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="canvas-container">
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Burger />
+        <OrbitControls />
+      </Canvas>
     </div>
   );
 }
